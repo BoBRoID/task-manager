043281036b1e9baa7331b8c7ed51c1a30a91b005
BoBRoID <n.gilko@gmail.com>
2019-08-29 19:39:39 +0300

    changes

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -5,13 +5,33 @@ namespace app\controllers;
 
 
 use app\models\Task;
-use yii\filters\AccessControl;
+use yii\data\DataFilter;
 use yii\rest\ActiveController;
 
 class TaskController extends ActiveController
 {
 
     public $modelClass = Task::class;
+
+    public function actions()
+    {
+        $defaultActions = parent::actions();
+
+        $defaultActions['index'] = [
+            'class'         =>  'yii\rest\IndexAction',
+            'modelClass'    =>  $this->modelClass,
+            'checkAccess'   =>  [$this, 'checkAccess'],
+            'dataFilter'    =>  [
+                'class'         =>  DataFilter::class,
+                'searchModel'   =>  function () {
+                    return (new \yii\base\DynamicModel(['isSolved' => 0]))
+                        ->addRule('isSolved', 'boolean');
+                },
+            ]
+        ];
+
+        return $defaultActions;
+    }
 
     public function behaviors()
     {
@@ -28,10 +48,13 @@ class TaskController extends ActiveController
             'corsFilter'    =>  [
                 'class' =>  \yii\filters\Cors::class,
                 'cors'  =>  [
-                    'Origin'    =>  ['http://localhost:8080', 'http://localhost:8081'],
-                    'Access-Control-Allow-Credentials' => true,
-                    'Access-Control-Request-Headers'   => ['content-type'],
-                    'Access-Control-Max-Age'           => 3600,
+                    'Origin'                            =>  ['http://localhost:8080', 'http://localhost:8081'],
+                    'Access-Control-Allow-Credentials'  =>  true,
+                    'Access-Control-Request-Headers'    =>  ['content-type'],
+                    'Access-Control-Max-Age'            =>  3600,
+                    'Access-Control-Allow-Methods'      =>  ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+                    'Access-Control-Request-Method'     =>  ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+
                 ]
             ]
         ];
--- views/site/index.php
+++ views/site/index.php
@@ -7,10 +7,10 @@ $this->title = 'Task manager';
 $this->registerCssFile('/static/css/app.820b4eed.css');
 $this->registerCssFile('/static/css/chunk-vendors.ee7084df.css');
 
-$this->registerLinkTag(['rel' => 'preload', 'as' => 'script', 'href' => '/static/js/app.2898cb0d.js']);
+$this->registerLinkTag(['rel' => 'preload', 'as' => 'script', 'href' => '/static/js/app.4f6d76f3.js']);
 $this->registerLinkTag(['rel' => 'preload', 'as' => 'script', 'href' => '/static/js/chunk-vendors.0c09bee5.js']);
 
 $this->registerJsFile('/static/js/chunk-vendors.0c09bee5.js');
-$this->registerJsFile('/static/js/app.2898cb0d.js');
+$this->registerJsFile('/static/js/app.4f6d76f3.js');
 ?>
 <div id="app"></div>
